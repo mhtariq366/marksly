e0de53c6dd2c447f1a836bb3f8e233921b46b968
Hassan <mhtariq366@gmail.com>
2024-12-30 00:30:08 +0500

    add institute init

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_institute/', views.add_institute, name='add_institute'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def add_institute(request):
+    return render(request, 'core/add_institute.html')
+
+
