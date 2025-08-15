ad91233a199acf4e3b5386d50f6f329985bf6b16
mhtariq366 <mhtariq366@gmail.com>
2025-08-15 21:36:47 +0500

    added templates folder in student app

--- student/views.py
+++ student/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse("Student Home Page")
+    return render(request, 'student/student_home.html')
