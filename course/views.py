80367c6f0ed5293990005fae4f92f8fcccc6ce57
mhtariq366 <mhtariq366@gmail.com>
2025-08-16 23:00:58 +0500

    added add course html and view, without styling

--- course/views.py
+++ course/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Course
 
 def course_home(request):
-    return render(request, 'course/course_home.html')
+    courses = Course.objects.all()
+    return render(request, 'course/course_home.html', {'courses': courses})
 
+def add_course(request):
+    if request.method == 'POST':
+        course_code = request.POST.get('course_code')
+        name = request.POST.get('name')
+        short_name = request.POST.get('short_name')
+        credit_hours = int(request.POST.get('credit_hours'))
+        remarks = request.POST.get('remarks')
+
+        Course.objects.create(
+            course_code = course_code,
+            name = name,
+            short_name = short_name,
+            credit_hours = credit_hours,
+            remarks = remarks
+        )
+
+        return redirect('course_home')
+    
+    return render(request, 'course/add_course.html')
