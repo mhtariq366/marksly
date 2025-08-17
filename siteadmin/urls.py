05effb907e478d8ec6d79da8d351da6cbc6f6f64
mhtariq366 <mhtariq366@gmail.com>
2025-08-17 22:40:39 +0500

    added add course

--- siteadmin/urls.py
+++ siteadmin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('add-course/', views.add_course, name='add_course'),
 ]
+
--- siteadmin/views.py
+++ siteadmin/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Course
 
-# Create your views here.
+def add_course(request):
+    if request.method == 'POST':
+        course_code = request.POST.get('course_code')
+        name = request.POST.get('name')
+        shortname = request.POST.get('shortname')
+        credit_hours = int(request.POST.get('credit_hours'))
+        remarks = request.POST.get('remarks')
+
+        Course.objects.create(
+            course_code = course_code,
+            name = name,
+            shortname = shortname,
+            credit_hours = credit_hours,
+            remarks = remarks
+        )
+
+        return redirect('add_course')
+    
+    return render(request, 'siteadmin/add_course.html')
