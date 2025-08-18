ddfb04123a5d74c7c84d5fcb7694d209b965a69c
mhtariq366 <mhtariq366@gmail.com>
2025-08-18 19:12:05 +0500

    course edit view, url and template added

--- siteadmin/urls.py
+++ siteadmin/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('add-course/', views.add_course, name='add_course'),
     path('course-list/', views.course_list, name='course_list'),
+    path('edit-course/<int:id>/', views.edit_course, name='edit_course'),
 ]
 
--- siteadmin/views.py
+++ siteadmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Course
 
 def add_course(request):
@@ -24,3 +24,18 @@ def add_course(request):
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'siteadmin/course_list.html', {'courses': courses})
+
+def edit_course(request, id):
+    course = get_object_or_404(Course, id=id)
+
+    if request.method == 'POST':
+        course.course_code = request.POST.get('course_code')
+        course.name = request.POST.get('name')
+        course.shortname = request.POST.get('shortname')
+        course.credit_hours = int(request.POST.get('credit_hours'))
+        course.remarks = request.POST.get('remarks')
+
+        course.save()
+        return redirect('course_list')
+
+    return render(request, 'siteadmin/edit_course.html', {'course': course})
