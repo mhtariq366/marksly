7b33aae1017cbb40b859c53067f238114c560a17
mhtariq366 <mhtariq366@gmail.com>
2025-08-18 19:36:32 +0500

    added teacher model in siteadmin

--- siteadmin/models.py
+++ siteadmin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Course(models.Model):
     course_code = models.CharField(max_length=20)
@@ -11,3 +12,11 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.course_code} - {self.name}"
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- siteadmin/urls.py
+++ siteadmin/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.course_list, name='course_list'),
     path('login/', views.siteadmin_login, name='siteadmin_login'),
     path('add-course/', views.add_course, name='add_course'),
-    path('course-list/', views.course_list, name='course_list'),
     path('edit-course/<int:id>/', views.edit_course, name='edit_course'),
 ]
 
